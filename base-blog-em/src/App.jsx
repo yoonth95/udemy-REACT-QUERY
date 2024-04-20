b3f151298a7a213f531d2488a4b88db15b35a950
윤태현 <yoonth0919@gmail.com>
2024-04-20 21:24:18 +0900

    Add: ReactQueryDevtools 추가

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,6 +1,7 @@
 import { Posts } from "./Posts";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
         <h1>Blog &apos;em Ipsum</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
